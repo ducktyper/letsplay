94d6e2aefceb6eb5bb618f914d1f36a4c6e45181
Ducksan Cho <ducktyper@gmail.com>
2015-10-20 23:12:00 +1300

    Build basic css, Modify index page

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Letsplay.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    post "/run", PageController, :run
   end
 
   # Other scopes may use custom stacks.
