9bc10a573bfeb8ec5566dc81aa66c4d4ae547fd0
Ducksan Cho <ducktyper@gmail.com>
2015-10-25 01:57:49 +1300

    manage katas from index.json and randomly select one on root page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,26 +2,56 @@ defmodule Letsplay.PageController do
   use Letsplay.Web, :controller
 
   def index(conn, _params) do
-    case File.read("web/test_codes/test_fibonacci.rb") do
-      {:ok, text} ->
-        render conn, "index.html", test: text, code: "", message: ""
-      {:error, reason} ->
-        render conn, "index.html", test: reason, code: "", message: ""
+    %{"name"          => name,
+      "language"      => language,
+      "test_filename" => test_filename,
+      "code_filename" => code_filename} =
+      File.read!("code_katas/index.json")
+      |> Poison.decode!
+      |> Enum.random
+
+    test_body = case File.read("code_katas/#{test_filename}") do
+      {:ok, text} -> text
+      _           -> ""
     end
+    code_body = case File.read("code_katas/#{code_filename}") do
+      {:ok, text} -> text
+      _           -> ""
+    end
+
+    render conn, "index.html",
+      code: %{filename: test_filename, body: code_body, language: "ruby"},
+      test: %{filename: code_filename, body: test_body},
+      name: name,
+      language: language,
+      output: ""
   end
 
-  def run(conn, %{"code" => code, "test" => test}) do
+  def run(conn, %{"code" => %{"filename" => code_filename, "body" => code_body},
+                  "test" => %{"filename" => test_filename, "body" => test_body},
+                  "language" => language, "name" => name}) do
     dir = System.cwd() <> "/tmp/" <>
       (:crypto.strong_rand_bytes(16) |> Base.url_encode64 |> binary_part(0, 16))
     File.rm_rf(dir)
     File.mkdir(dir)
-    File.write(dir <> "/test_fibonacci.rb", test)
-    File.write(dir <> "/fibonacci.rb", code)
-    case System.cmd("docker", ["run", "-v", dir <> ":/home/app",
-      "letsplay/ruby", "ruby", "test_fibonacci.rb"], stderr_to_stdout: true) do
-      {std, _} ->
+    File.write("#{dir}/#{code_filename}", code_body)
+    File.write("#{dir}/#{test_filename}", test_body)
+
+    cmd = ["run", "-v", dir <> ":/home/app", "letsplay/#{language}"]
+    cmd = case language do
+      "ruby" -> cmd ++ ["ruby", test_filename]
+      _ -> cmd ++ ["ruby", "echo", "nosupport"]
+    end
+
+    case System.cmd("docker", cmd, stderr_to_stdout: true) do
+      {output, _} ->
         File.rm_rf(dir)
-        render conn, "index.html", test: test, code: code, message: std
+        render conn, "index.html",
+          code: %{filename: code_filename, body: code_body},
+          test: %{filename: test_filename, body: test_body},
+          name: name,
+          language: language,
+          output: output
     end
   end
 end
